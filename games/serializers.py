97a9dd516d1627ce415323a1c559a2ab73916849
Illia Matsykur <matsykur.illya@gmail.com>
2026-06-10 22:02:52 +0200

    optimized api

--- games/serializers.py
+++ games/serializers.py
@@ -1,13 +1,14 @@
 from rest_framework import serializers
 from .models import Category, Game
 
-class GameSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Game
-        fields = ('id', 'title', 'slug', 'logo')
-
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ('id', 'title', 'slug', 'game')
+
+class GameSerializer(serializers.ModelSerializer):
+    categories = CategorySerializer(many=True, read_only=True, source='category_set')
+    class Meta:
+        model = Game
+        fields = ('id', 'title', 'slug', 'logo', 'categories')
 

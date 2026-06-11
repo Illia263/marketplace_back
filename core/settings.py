5c6f8c57d4d0a45a5af73bead16b0980e4b8aeef
Illia Matsykur <matsykur.illya@gmail.com>
2026-06-11 04:20:23 +0200

    added filters and pagination

--- core/settings.py
+++ core/settings.py
@@ -118,6 +118,8 @@ STATIC_URL = 'static/'
 AUTH_USER_MODEL = 'users.CustomUser'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication', 
         'rest_framework.authentication.TokenAuthentication',
